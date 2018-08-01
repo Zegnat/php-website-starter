740e679c5c69a9d94c8fd57a120de715ab312ab3
Martijn van der Ven <martijn@vanderven.se>
2018-08-01 15:50:42 +0200

    No specific HTTP PSR implementation required
    Use a standalone ServerRequest builder based on configurable PSR-17
    implementations to start of the middleware dispatcher. For now the
    default is to use a third-party PSR-17 implementation for Zend’s
    Diactoros PSR-7 objects.

--- config/injector.php
+++ config/injector.php
@@ -1,1 +1,19 @@
 <?php
+
+// Configure PSR-17 factories:
+$i->alias(
+    Psr\Http\Message\ServerRequestFactoryInterface::class,
+    Http\Factory\Diactoros\ServerRequestFactory::class
+);
+$i->alias(
+    Psr\Http\Message\StreamFactoryInterface::class,
+    Http\Factory\Diactoros\StreamFactory::class
+);
+$i->alias(
+    Psr\Http\Message\UploadedFileFactoryInterface::class,
+    Http\Factory\Diactoros\UploadedFileFactory::class
+);
+$i->alias(
+    Psr\Http\Message\UriFactoryInterface::class,
+    Http\Factory\Diactoros\UriFactory::class
+);
