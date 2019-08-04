479955755c3b4aba6a318019f922919a4840dfb3
Martijn van der Ven <martijn@vanderven.se>
2019-08-04 13:38:04 +0200

    Add some default PHPUnit to encourage tests

---
+++ tests/RequestHandler/HomeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\RequestHandler;
+
+class HomeTest extends \PHPUnit\Framework\TestCase {
+    public function testCanConstruct(): Home {
+        $home = new Home(new \Zend\Diactoros\ResponseFactory());
+        $this->assertInstanceOf(Home::class, $home);
+        return $home;
+    }
+
+    /**
+     * @depends testCanConstruct
+     */
+    public function testReturnsOK(Home $home) {
+        $response = $home->handle(new \Zend\Diactoros\ServerRequest());
+        $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+    }
+}
