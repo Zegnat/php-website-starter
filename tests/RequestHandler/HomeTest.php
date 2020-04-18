bf356ee392b4b9dab75ef723a6c24d69695c52eb
Martijn van der Ven <martijn@vanderven.se>
2020-04-18 15:29:07 +0200

    Apply styleguide to test

--- tests/RequestHandler/HomeTest.php
+++ tests/RequestHandler/HomeTest.php
@@ -1,18 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace app\RequestHandler;
 
-class HomeTest extends \PHPUnit\Framework\TestCase {
-    public function testCanConstruct(): Home {
+class HomeTest extends \PHPUnit\Framework\TestCase
+{
+    public function testCanConstruct(): Home
+    {
         $home = new Home(new \Zend\Diactoros\ResponseFactory());
         $this->assertInstanceOf(Home::class, $home);
+
         return $home;
     }
 
     /**
      * @depends testCanConstruct
      */
-    public function testReturnsOK(Home $home) {
+    public function testReturnsOK(Home $home)
+    {
         $response = $home->handle(new \Zend\Diactoros\ServerRequest());
         $this->assertInstanceOf(\Psr\Http\Message\ResponseInterface::class, $response);
         $this->assertSame(200, $response->getStatusCode());
